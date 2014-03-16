2ca5c8a39e02993c838374fc9f713521b2b6cbae
Andrew Reed <reed@cs.unc.edu>
2014-03-15 23:22:57 -0400

    First real commit.

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-ProfileMaker
-============
+## _ProfileMaker_
 
-A utility to create profiles of Netflix videos.
+A utility to create profiles of Netflix videos that can be used in _dashem_.
+
+_ProfileMaker_ uses a video's manifest to obtain the URLs for each bitrate encoding, but only 
+downloads the header of the 3000 Kbps encoding's .ismv. This header is then used to 
+calculate the segment sizes for the standard segment duration (4 seconds), as well as the segment 
+sizes if Netflix were to use 2, 8, and 16 second segments.
+
+### Instructions
+
+1. Create an empty XML file.
+2. Install the Firebug plugin for Firefox.
+3. Open Firefox and go to Netflix.
+4. Open the Firebug plugin.
+5. Play a Netflix video.
+6. Inside Firebug, right click the line that says __POST authorization__. Select _Copy Response Body_.
+7. Paste the response body into the XML file. Save and close the file.
+8. Run ProfileMaker (for this example, assume that the video is _The Hunger Games_:
+
+		java -jar ProfileMaker.jar > hungergames.txt
+
+9. Type the filename of the XML file and hit enter.
+10. Now, use the following commands to extract the profiles for the various segment durations:
+
+		sed -n '1p' hungergames.txt | tr ',' '\n' > hungergames_2.txt
+		sed -n '2p' hungergames.txt | tr ',' '\n' > hungergames_4.txt
+		sed -n '3p' hungergames.txt | tr ',' '\n' > hungergames_8.txt
+		sed -n '4p' hungergames.txt | tr ',' '\n' > hungergames_16.txt
+
+11. Cut and paste the four profiles into their respective destinations (e.g. /var/www/dashemulatordata/netflix_2sec/videos/).
+12. Rename the files to remove the trailing "_#".
+
+### Credit / Copying
+
+As a work of the United States Government, _SignatureMaker_ is 
+in the public domain within the United States. Additionally, 
+Andrew Reed waives copyright and related rights in the work 
+worldwide through the CC0 1.0 Universal public domain dedication 
+(which can be found at http://creativecommons.org/publicdomain/zero/1.0/).
---
+++ src/ProfileMaker.java
@@ -0,0 +1,122 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+import java.net.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+
+public class ProfileMaker {
+
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		String pathToManifest = sc.nextLine();
+
+		Encoding encoding = retrieveEncodings(pathToManifest);
+
+		int[] segmentSizes;
+
+		segmentSizes = encoding.getSegmentSizes_2s();
+
+		for (int i = 0; i < segmentSizes.length; i++) {
+			System.out.print(segmentSizes[i]);
+			System.out.print(",");
+		}
+
+		System.out.print("\n");
+
+		segmentSizes = encoding.getSegmentSizes_4s();
+
+		for (int i = 0; i < segmentSizes.length; i++) {
+			System.out.print(segmentSizes[i]);
+			System.out.print(",");
+		}
+
+		System.out.print("\n");
+
+		segmentSizes = encoding.getSegmentSizes_8s();
+
+		for (int i = 0; i < segmentSizes.length; i++) {
+			System.out.print(segmentSizes[i]);
+			System.out.print(",");
+		}
+
+		System.out.print("\n");
+
+		segmentSizes = encoding.getSegmentSizes_16s();
+
+		for (int i = 0; i < segmentSizes.length; i++) {
+			System.out.print(segmentSizes[i]);
+			System.out.print(",");
+		}
+
+		sc.close();
+	}
+
+	private static Encoding retrieveEncodings(String pathToManifest) {
+		Encoding encoding = null;
+		boolean found_3000 = false;		
+
+		try {
+			File xmlFile = new File(pathToManifest);
+			String xmlString = new String("");
+
+			Scanner sc = new Scanner(xmlFile);
+
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+
+				if (line.contains("<HMAC>")) {
+					continue;
+				}
+				else {
+					xmlString += line + "\n";
+				}
+			}
+
+			sc.close();
+
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			InputSource is = new InputSource(new StringReader(xmlString));
+			Document doc = dBuilder.parse(is);
+
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("nccp:videodownloadable");
+
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+ 
+				Node nNode = nList.item(temp);
+ 
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+ 
+					Element eElement = (Element) nNode;
+ 
+					String bitrate = eElement.getElementsByTagName("nccp:bitrate").item(0).getTextContent();
+					String url = eElement.getElementsByTagName("nccp:url").item(0).getTextContent();
+
+					if (bitrate.equals("3000")) {
+						encoding = new Encoding(bitrate, url);
+						found_3000 = true;
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("ERROR: Unable to retrieve encodings.");
+			System.exit(0);
+		}
+
+		if (!found_3000) {
+			System.out.println("ERROR: No 3000 Kbps bitrate.");
+			System.exit(0);
+		}
+
+		return encoding;
+	}
+}
